2737eae913bd558f6e6a8dbdab20adc4b370e239
Hongxiang Zheng <hxzheng2003@gmail.com>
2025-03-13 20:23:53 -0700

    Update the create_indexes.sql

--- cs166_project/sql/src/create_indexes.sql
+++ cs166_project/sql/src/create_indexes.sql
@@ -1,8 +1,15 @@
--- Index for FoodOrder.login (it speeds up user-related queries)
+-- Index on FoodOrder.login to speed up lookups when retrieving user orders
+-- Helps when the user wants to view their order history
 CREATE INDEX idx_foodorder_login ON FoodOrder(login);
 
--- Index for FoodOrder.storeID (it speeds up store lookups since there are 1000 stores)
-CREATE INDEX idx_foodorder_storeID ON FoodOrder(storeID);
+-- Helps when the user wants to view their latest order
+-- avoids full table scan
+CREATE INDEX idx_foodorder_timestamp ON FoodOrder(orderTimestamp DESC);
 
--- Index for ItemsInOrder.orderID (it speeds up order-item lookups)
+-- Speed up lookups when retrieving items in an order
 CREATE INDEX idx_itemsinorder_orderID ON ItemsInOrder(orderID);
+
+-- Speeds up store lookups when filtering by storeID.
+-- Reduces the number of rows scanned when checking available stores
+CREATE INDEX idx_store_storeID ON Store(storeID);
+
